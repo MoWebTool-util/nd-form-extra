93cd7b8db6a5393c65e0da39f82e9adc6599653f
crossjs <liwenfu@crossjs.com>
2015-05-13 19:19:52 +0800

    update

--- index.js
+++ index.js
@@ -6,71 +6,17 @@
 'use strict';
 
 var Form = require('nd-form');
-var Calendar = require('nd-calendar');
 var Select = require('nd-select');
+var Calendar = require('nd-calendar');
+var Editor = require('nd-editor');
 var Upload = require('nd-upload');
 var Validator = require('nd-validator');
 
 module.exports = Form.extend({
 
-  Plugins: [Calendar, Select, Validator, Upload],
+  Plugins: [Select, Calendar, Editor, Upload, Validator],
 
   attrs: {
-    pluginCfg: {
-      Calendar: {
-        listeners: {
-          start: function() {
-            this.host.after('addField', this.execute);
-          }
-        }
-      },
-      Select: {
-        listeners: {
-          start: function() {
-            this.host.after('addField', this.execute);
-          }
-        }
-      },
-      Validator: {
-        listeners: {
-          start: function() {
-            var plugin = this;
-
-            plugin.on('export', function(instance) {
-              // 异步，添加到队列
-              plugin.host.queue.use(function(next) {
-                instance.execute(function(err) {
-                  if (!err) {
-                    next();
-                  }
-                });
-              });
-            });
-          }
-        }
-      },
-      Upload: {
-        listeners: {
-          start: function() {
-            var plugin = this;
-
-            plugin.on('export', function(instance) {
-              // 异步，添加到队列
-              plugin.host.queue.use(function(next) {
-                instance.execute(function(err) {
-                  if (!err) {
-                    next();
-                  }
-                });
-              });
-            });
-
-            this.host.after('addField', this.execute);
-          }
-        }
-      }
-    },
-
     buttons: [{
       label: '取消',
       type: 'button',
